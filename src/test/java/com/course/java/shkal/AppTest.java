0a0d9fdb7e8e43f7a9a9ad92aa1bcefbcd757b7d
Shkal <shkaaal@mail.ru>
2022-09-21 19:23:31 +0300

    Create Project

---
+++ src/test/java/com/course/java/shkal/AppTest.java
@@ -0,0 +1,12 @@
+package com.course.java.shkal;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
